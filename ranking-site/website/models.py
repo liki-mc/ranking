1073e6306ff9f467b997501d4b2dcf464043e15f
liki-mc <liki.mc@outlook.com>
2025-01-23 19:51:44 +0100

    changes

--- ranking-site/website/models.py
+++ ranking-site/website/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Ranking(models.Model):
-    name = models.CharField(max_length = 200)
-    rid = models.IntegerField(primary_key = True)
-    character = models.CharField(max_length = 200)
+    name = models.CharField(max_length = 200, blank = False)
+    rid = models.AutoField(primary_key = True)
+    character = models.CharField(max_length = 200, default = "+")
     channel = models.IntegerField()
     date = models.DateTimeField("date published", auto_now_add = True)
     
@@ -13,8 +13,8 @@ class Ranking(models.Model):
 
 class Entry(models.Model):
     ranking = models.ForeignKey(Ranking, on_delete = models.CASCADE)
-    number = models.IntegerField()
-    user = models.IntegerField()
+    number = models.IntegerField(default = 1)
+    user = models.IntegerField(blank = False)
     date = models.DateTimeField("date published", auto_now_add = True)
     
     def __str__(self):
