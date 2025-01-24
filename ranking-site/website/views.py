586be75dbff8a8052873bcf40bd1774ca6f988f9
Cédric Van Cauwenberghe <Cedric.VanCauwenberghe@UGent.be>
2025-01-24 23:25:22 +0100

    Start of pages

--- ranking-site/website/views.py
+++ ranking-site/website/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from django.http import HttpResponse
 
-
+from .models import Ranking
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    args = {}
+    args['rankings'] = Ranking.objects.all()
+    return render(request, "website/index.html",args)
+
+
+def ranking_detail(request, rid):
+    args = {}
+    args['ranking'] = get_object_or_404(Ranking, pk=rid)
+    return render(request, "website/ranking_detail.html", args)
